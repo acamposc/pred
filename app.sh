aa7d082380097bcc74f53b989b27eb706395a17a
Arturo Campos <arturplur@gmail.com>
2020-04-17 04:19:55 -0500

    mods on the front

---
+++ app.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#########################################################
+
+#activate / deactivate virtual environment
+#https://www.youtube.com/watch?v=Kg1Yvry_Ydk
+#python3 -m venv pred/venv
+#source pred/venv/bin/activate
+#deactivate
+
+
+# install requirements.txt
+##pip install -r requirements.txt
+
+#create or replace bigquery views.
+cd appweb
+dbt run --vars '{"date_begin":"20200401", "date_end":"20200404"}
